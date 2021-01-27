9e0e81db7e7fac37bf0d559ae71ac91bcf334c13
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-27 21:34:02 +0200

    Retrieves the videos using their name using spring data.

--- src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
+++ src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
@@ -7,4 +7,7 @@ public interface VideoRepository extends CrudRepository<Video, Long>{
 	
 	// return the video by using ID.
 	Video findById(Long id);
+	
+	// return the video using their name.
+	Collection<Video> findByName(String name);
 }
