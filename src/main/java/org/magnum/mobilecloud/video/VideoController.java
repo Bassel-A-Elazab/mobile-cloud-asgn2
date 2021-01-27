90a8ca2a91b3ee37e6c2f714000ac92e79e14c68
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-27 21:43:07 +0200

    Add annotations thats required for this controller which first is @RestController to use rest over http control and @RequestMapping to map a client request to specific controller, The last thing is Create an instance of VideoRepository that uses spring data but there is a little bit difference which is autowired thats means is automatically inject the instance into variable at runtime.

--- src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -1,5 +1,17 @@
 package org.magnum.mobilecloud.video;
 
-public class VideoController {
+import org.magnum.mobilecloud.video.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value="/video")
+public class VideoController {
+	
+	@Autowired
+	private VideoRepository videoRep; 
+	
+	
 }
