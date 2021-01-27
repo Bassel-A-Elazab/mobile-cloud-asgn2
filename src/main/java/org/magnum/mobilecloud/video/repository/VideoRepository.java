cada763f4de1b005b7cd59a1a0ac9bbf7adda8f2
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-27 21:30:07 +0200

    Creates Interface that implements Spring Data JPA that applying CRUD operations for video controller

---
+++ src/main/java/org/magnum/mobilecloud/video/repository/VideoRepository.java
@@ -0,0 +1,8 @@
+package org.magnum.mobilecloud.video.repository;
+
+import java.util.Collection;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VideoRepository extends CrudRepository<Video, Long>{
+	
+}
