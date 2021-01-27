291b2454af4e4b675ddfbf2b29a90db5fee23706
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-27 21:54:29 +0200

    Requestmapping of retrieves all videos that using spring data JPA.

--- src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -1,10 +1,17 @@
 package org.magnum.mobilecloud.video;
 
+import java.util.Collection;
+
+import org.magnum.mobilecloud.video.repository.Video;
 import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.common.collect.Lists;
 
 @RestController
 @RequestMapping(value="/video")
@@ -13,5 +20,10 @@ public class VideoController {
 	@Autowired
 	private VideoRepository videoRep; 
 	
+	// Mapping a request of /video to get all videos using spring data.
+	@RequestMapping(method=RequestMethod.GET)
+	public @ResponseBody Collection<Video> getAllVideos(){
+		return Lists.newArrayList(videoRep.findAll());
+	}
 	
 }
