a1bcb4683441b0579c0929938c039edcdf314d4d
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-27 22:04:53 +0200

    RequestMapping of post video to add new video using spring data JPA.

--- src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -6,6 +6,7 @@ import org.magnum.mobilecloud.video.repository.Video;
 import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,10 +21,16 @@ public class VideoController {
 	@Autowired
 	private VideoRepository videoRep; 
 	
-	// Mapping a request of /video to get all videos using spring data.
+	// Mapping a request of /video to get all videos using spring data JPA.
 	@RequestMapping(method=RequestMethod.GET)
 	public @ResponseBody Collection<Video> getAllVideos(){
 		return Lists.newArrayList(videoRep.findAll());
+	}
+	
+	// Mapping a request of /video to add new video using spring data JPA.
+	@RequestMapping(method = RequestMethod.POST)
+	public @ResponseBody Video addVideo(@RequestBody Video video) {
+		return videoRep.save(video);
 	}
 	
 }
