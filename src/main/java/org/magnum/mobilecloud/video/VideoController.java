43087e066973e95b725034591f0d52bd082c0a99
Bassel Elazab <bassel.alazab@gmail.com>
2021-01-29 23:37:59 +0200

    Import the not found class from client package.

--- src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -2,10 +2,11 @@ package org.magnum.mobilecloud.video;
 
 import java.util.Collection;
 
+import org.magnum.mobilecloud.video.client.VideoNotFoundException;
 import org.magnum.mobilecloud.video.repository.Video;
 import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
